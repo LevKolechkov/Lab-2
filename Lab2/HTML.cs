2650cc7b2c30e9a8f9f6763572a9075675016b6b
LevKolechkov <levkolechkov@gmail.com>
2023-02-19 18:42:52 +0700

    Первый коммит

---
+++ Lab2/HTML.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Lab2
+{
+  class HTML : Doc_class
+  {
+    string ID;
+    string classes;
+
+    public override void PrintInfo()
+    {
+      Console.WriteLine($" Имя = {name}; \n Автор = {author}; \n Ключевые слова = {keyWords}; \n Тема = {theme}; \n Путь = {path}; \n ID = {ID}; \n Классы {classes}.");
+    }
+
+    public HTML (string name, string author, string keyWords, string theme, string path, string ID, string classes)
+    {
+      this.name = name;
+      this.author = author;
+      this.keyWords = keyWords;
+      this.theme = theme;
+      this.path = path;
+      this.ID = ID;
+      this.classes = classes;
+    }
+  }
+}
