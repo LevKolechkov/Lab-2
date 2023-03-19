c0b905637523fe2606b94dbbf49097e27b111d05
LevKolechkov <levkolechkov@gmail.com>
2023-03-19 22:56:17 +0700

    Первые наброски

--- Lab3/Execution.cs
+++ Lab3/Execution.cs
@@ -1,152 +1,26 @@
-﻿/***************************
-*      Колечков Лев        *
-*       13.03.2023         *
-* Стандартный ввод и вывод *
-***************************/
-using Lab4;
-using System;
+﻿using System;
 using System.IO;
 
-namespace Lab4
+namespace Lab2
 {
   class Execution
   {
-    static void Main(string[] args)
+    public void Main(string[] args)
     {
-      KeyWords keyWords = new KeyWords();
+      string wrongLink = ("D:\\Workspace\\Labs\\Файлы\\wrongWords.txt");
+      string rightLink = ("D:\\Workspace\\Labs\\Файлы\\rightWords.txt");
 
-      while (true)
-      {
-        Console.WriteLine("Начнём с создания файла с описанием вашего кота");
+      FileStream wrongFile = new FileStream(wrongLink, FileMode.OpenOrCreate, FileAccess.Read);
 
-        Cat catFile = new Cat();
+      FileStream rightFile = new FileStream(rightLink, FileMode.OpenOrCreate, FileAccess.Write);
 
-        Console.WriteLine("Введите название породы");
+      int count = File.ReadAllLines(wrongLink).Length;
 
-        catFile.Breed = Console.ReadLine();
+      WrongWords wrongWords = new WrongWords(count);
 
-        keyWords.addKeyWord(catFile.Breed);
+      wrongWords.AddWords();
 
-        Console.WriteLine("Введите имя кота");
-
-        catFile.Name = Console.ReadLine();
-
-        keyWords.addKeyWord(catFile.Name);
-
-        Console.WriteLine("Теперь введите возраст кота");
-
-        catFile.Age = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Выберите как Вы хотите сериализовать файл: 1 - Bin; 2 - XML");
-
-        int choiceSerialize = int.Parse(Console.ReadLine());
-
-        switch (choiceSerialize)
-        {
-          case 1:
-            keyWords.addKeyWord("Bin");
-
-            Console.WriteLine("Вы выбрали Bin");
-
-            FileStream binCat = new FileStream($"D:\\Workspace\\Labs\\Файлы\\BinCat{catFile.Breed}{catFile.Name}", FileMode.OpenOrCreate, FileAccess.Write);
-
-            catFile.binSerialize(binCat);
-
-            break;
-
-          case 2:
-            keyWords.addKeyWord("XML");
-
-            Console.WriteLine("Вы выбрали XML");
-
-            FileStream xmlCat = new FileStream($"D:\\Workspace\\Labs\\Файлы\\XmlCat{catFile.Breed}{catFile.Name}", FileMode.OpenOrCreate, FileAccess.Write);
-
-            catFile.XmlSerialize(xmlCat);
-
-            break;
-
-          default:
-
-            throw new Exception("Данного варианта нет. Попрошу начать заново");
-        }
-
-        Console.WriteLine("Желаете ли вы создать ещё один файл? (Да/Нет)");
-
-        string choiceSerFile = Console.ReadLine();
-
-        if (choiceSerFile == "Нет") break;
-        else Console.Clear();
-      }
-
-      string serKey, breedKey, nameKey;
-
-      FileStream fileCat = keyWords.FindFile(out serKey, out breedKey, out nameKey);
-
-      Console.WriteLine("Что вы хотите с ним сделать?");
-      Console.WriteLine("1 - Вывести. 2 - Перезаписать");
-
-      int choiceFile = int.Parse(Console.ReadLine());
-
-      Cat cat = new Cat();
-
-      if (serKey == "Bin")
-      {
-        cat.binSerialize(fileCat);
-
-      }
-      else
-      {
-        cat.XmlDeserialize(fileCat);
-
-      }
-
-      Caretaker saveCat = new Caretaker();
-
-      switch (choiceFile)
-      {
-        case 1:
-          Console.WriteLine("Вы выбрали вывести");
-
-          cat.Print();
-
-          break;
-
-        case 2:
-          Console.WriteLine("Вы выбрали перезаписать");
-
-          saveCat.SaveState(cat);
-
-          Console.WriteLine("Введите название породы");
-          cat.Breed = Console.ReadLine();
-
-          Console.WriteLine("Введите имя");
-          cat.Name = Console.ReadLine();
-
-          Console.WriteLine("Введите возраст");
-          cat.Age = int.Parse(Console.ReadLine());
-
-          Console.WriteLine("Вы изменили на:");
-          cat.Print();
-
-          Console.WriteLine("Желаете ли отменить? (Да/Нет)");
-
-          string choiceCancel = Console.ReadLine();
-
-          if (choiceCancel == "Да")
-          {
-            saveCat.RestoreState(cat);
-
-            Console.WriteLine("Изменения отменены");
-
-            cat.Print();
-          }
-          else
-          {
-            cat.Print();
-          }
-
-          break;
-      }
+       
     }
   }
 }
---
+++ Lab3/WrongWords.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2
+{
+  class WrongWords
+  {
+    public WrongWords()
+    {
+
+    }
+
+    public WrongWords(int count) 
+    {
+      this.count = count;
+    }
+
+    private int count;
+
+    Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+
+    public void AddWords()
+    {
+      for (int rowIndex = 0; rowIndex <= count; ++rowIndex)
+      {
+        Console.WriteLine("Введите правильное слово");
+
+        string rightWord = Console.ReadLine();
+
+        List<string> wrongWords = new List<string>();
+
+        Console.WriteLine("Введите теперь к этому слову неправильные слова");
+
+        for (int columnIndex = 0; columnIndex <= 2; ++columnIndex) 
+        {
+          wrongWords.Add(Console.ReadLine());
+        }
+
+        dictionary.Add(rightWord, wrongWords);
+      }
+    }
+  }
+}
